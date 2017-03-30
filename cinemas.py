69ef344ab2c3e70c71b8ba61d002f22208d46b13
Nikita Sapunov <kiton1994@gmail.com>
2017-03-30 12:35:57 +0300

    Structure improvements

--- cinemas.py
+++ cinemas.py
@@ -1,105 +1,21 @@
 #!/usr/bin/env python3
 
 import os
-import errno
-import time
 import requests
 import json
 import argparse
-import tempfile
-import shutil
-import statistics
-from hashlib import sha1
 
-import msgpack
+import cacher as cache
 from bs4 import BeautifulSoup
 
-from json.decoder import JSONDecodeError
+
+CACHE_LIFETIME = 60 * 60 * 24 * 7
+SIMILARITY_THRESHOLD = 3
+
+cache.set_cache_directory(os.path.join(os.path.dirname(__file__), ".cache"))
 
 
-class Cacher:
-    objects = {}
-    cacher_dirname = "cacher"
-
-    def __init__(self, cache_name):
-        self.cache_path = os.path.join(
-            tempfile.gettempdir(), self.cacher_dirname, cache_name)
-        self._ensure_dir(self.cache_path)
-
-    def put(self, name, obj, expires=60):
-        self.objects[name] = {
-            "data": obj,
-            "expires": self._now() + expires
-        }
-
-        self._write(os.path.join(self.cache_path, name), self.objects[name])
-
-        return self.objects[name]["data"]
-
-    def get(self, name):
-        filepath = os.path.join(self.cache_path, name)
-
-        if name not in self.objects:
-            if os.path.exists(filepath):
-                self.objects[name] = self._read(filepath)
-            else:
-                return None
-
-        if self.objects[name]["expires"] <= self._now():
-            try:
-                os.remove(filepath)
-            except FileNotFoundError:
-                pass
-
-            return None
-        else:
-            return self.objects[name]["data"]
-
-    def remove(self, name):
-        filepath = os.path.join(self.cache_path, name)
-
-        try:
-            del self.objects[name]
-        except KeyError:
-            pass
-
-        try:
-            os.remove(filepath)
-        except FileNotFoundError:
-            pass
-
-    def remove_all(self):
-        try:
-            shutil.rmtree(self.cache_path)
-        except FileNotFoundError:
-            pass
-
-        self._ensure_dir(self.cache_path)
-        self.objects = {}
-
-    def _now(self):
-        return int(time.time())
-
-    def _write(self, filepath, obj):
-        with open(filepath, "wb") as cache_file:
-            msgpack.pack(obj, cache_file)
-
-    def _read(self, filepath):
-        with open(filepath, "rb") as cache_file:
-            return msgpack.unpack(cache_file, encoding="utf-8")
-
-    def _ensure_dir(self, dirname):
-        try:
-            os.makedirs(dirname)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                raise
-
-
-CACHE = Cacher("cinemas")
-
-
-def _levenshtein_distance(a, b):
+def levenshtein_distance(a, b):
     n, m = len(a), len(b)
 
     if n > m:
@@ -121,10 +37,10 @@ def _levenshtein_distance(a, b):
 
 
 def fetch_page(url, headers=None):
-    url_hash = "request-" + sha1(bytes(url, "utf-8")).hexdigest()
 
-    ans = CACHE.get(url_hash)
-    if ans is None:
+    data = cache.get(url)
+
+    if data is None:
         try:
             page = requests.get(url, headers=headers)
         except requests.exceptions.ConnectionError:
@@ -132,40 +48,46 @@ def fetch_page(url, headers=None):
 
         if page.status_code == 200:
             try:
-                ans = page.json()
-            except JSONDecodeError:
-                ans = page.text
+                data = page.json()
+            except ValueError:
+                data = page.text
 
-            CACHE.put(url_hash, ans, 60 * 60 * 24)
+            cache.put(url, data, CACHE_LIFETIME)
 
-    return ans
+    return data
 
 
 def fetch_afisha_page():
+
     afisha_page = "http://www.afisha.ru/msk/schedule_cinema"
 
     return fetch_page(afisha_page)
 
 
 def fetch_kinozal_data(movie_title):
+
     kinozal_page = "https://suggest-kinopoisk.yandex.net/" \
         "suggest-kinopoisk?srv=kinopoisk&part={0}"
 
-    user_agent = ("Mozilla/5.0 (X11; Linux x86_64) ",
-                  "AppleWebKit/537.36 ",
-                  "(KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36")
+    user_agent = " ".join((
+        "Mozilla/5.0 (X11; Linux x86_64)",
+        "AppleWebKit/537.36",
+        "(KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"
+    ))
+
     headers = {
         "Host": "suggest-kinopoisk.yandex.net",
         "Origin": "https://plus.kinopoisk.ru",
         "Pragma": "no-cache",
         "Referer": "https://plus.kinopoisk.ru/",
-        "User-Agent": "".join(user_agent)
+        "User-Agent": user_agent
     }
 
     return fetch_page(kinozal_page.format(movie_title), headers)
 
 
 def parse_afisha_list(raw_html):
+
     soup = BeautifulSoup(raw_html, "lxml")
     movies = {}
 
@@ -197,7 +119,7 @@ def parse_afisha_list(raw_html):
         kinopoisk_data = fetch_movie_info(movies[i]["name"])
         a, b = kinopoisk_data["name"].lower(), movies[i]["name"].lower()
 
-        if a == b or _levenshtein_distance(a, b) <= 3:
+        if a == b or levenshtein_distance(a, b) <= SIMILARITY_THRESHOLD:
             movies[i].update(kinopoisk_data)
 
         # To reduce problems with sorting
@@ -208,6 +130,7 @@ def parse_afisha_list(raw_html):
 
 
 def interpret_kinopoisk_search_results(suggest_result):
+
     res = {"name": "", "votes": 0, "rate": 0}
 
     try:
@@ -223,16 +146,19 @@ def interpret_kinopoisk_search_results(suggest_result):
 
 
 def fetch_movie_info(movie_title):
+
     movie_title = movie_title.lower()
 
     return interpret_kinopoisk_search_results(fetch_kinozal_data(movie_title))
 
 
 def _rate_with_votes(rate, votes, top, bottom):
+
     return rate + (votes - bottom) / (top - bottom) * 10
 
 
-def output_movies_to_console(movies, count, most_cinemas, take_votes):
+def console_output(movies, count, most_cinemas, take_votes):
+
     if take_votes:
         votes = [it["votes"] for it in movies]
         max_votes = max(votes)
@@ -244,18 +170,19 @@ def output_movies_to_console(movies, count, most_cinemas, take_votes):
         movies.sort(key=lambda it: it["rate"], reverse=True)
 
     if most_cinemas:
-        cinemas = [it["cinemas_count"] for it in movies]
-        cinemas_mean = statistics.mean(cinemas)
+        cinemas_count = [it["cinemas_count"] for it in movies]
+        cinemas_mean = sum(cinemas_count) / len(cinemas_count)
 
-        movies = list(filter(
-            lambda it: it["cinemas_count"] > cinemas_mean, movies))
+        movies = [it for it in movies if it["cinemas_count"] > cinemas_mean]
 
-    movies = list(filter(lambda it: it["rate"] != 0, movies))
+    movies = [it for it in movies if it["rate"] != 0]
 
     format_str = "{0:<4}{1:<50}{2:<7}{3:<8}{4}"
     print("-" * 76)
     print(format_str.format("#", "Movie", "Rate", "Votes", "Cinemas"))
     print("-" * 76)
+
+    count = count if count != 0 else len(movies)
 
     for i, movie in enumerate(movies[:count]):
         print(format_str.format(
@@ -268,23 +195,8 @@ def output_movies_to_console(movies, count, most_cinemas, take_votes):
     print("-" * 76)
 
 
-def main(movies_count, most_cinemas, take_votes, remove_cache):
-    if remove_cache:
-        CACHE.remove_all()
+def main():
 
-    popular_movies = CACHE.get("popular_movies")
-    if popular_movies is None:
-        print("% Data will be downloaded from internet. This may take a while")
-
-        popular_movies = CACHE.put(
-            "popular_movies",
-            parse_afisha_list(fetch_afisha_page()), 60 * 60 * 24 * 7)
-
-    output_movies_to_console(
-        list(popular_movies.values()), movies_count, most_cinemas, take_votes)
-
-
-if __name__ == "__main__":
     description = "Get popular movies with highest kinopoisk rate"
 
     parser = argparse.ArgumentParser(description=description)
@@ -305,8 +217,29 @@ if __name__ == "__main__":
         "--clean-cache",
         action="store_true",
         help="Remove cached data. New data will be downloaded from \
-            afisha and kinopoisk")
+            afisha.ru and kinopoisk.ru")
 
     args = parser.parse_args()
 
-    main(args.number, args.most_cinemas, args.votes, args.clean_cache)
+    if args.clean_cache:
+        cache.remove_all()
+
+    popular_movies = cache.get("popular_movies")
+
+    if popular_movies is None:
+        print("% Data will be downloaded from Internet. This may take a while")
+
+        popular_movies = cache.put(
+            "popular_movies",
+            parse_afisha_list(fetch_afisha_page()), CACHE_LIFETIME)
+
+    console_output(
+        list(popular_movies.values()),
+        args.number,
+        args.most_cinemas,
+        args.votes
+    )
+
+
+if __name__ == "__main__":
+    main()
